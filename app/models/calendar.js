fc304e5b473709595c36d7dc4a5a7e0196041fbb
Nikki X <djknitex@outlook.com>
2018-09-10 22:18:49 -0700

    added test code for business-days-calculator

--- app/models/calendar.js
+++ app/models/calendar.js
@@ -1,3 +1,8 @@
-module.exports = {
-    
-}
+const calculator = require("business-days-calculator");
+const calendar = require("holidays-calendar");
+calculator.SetCalendar(calendar);
+ 
+let today = new Date();
+console.log('Today '+ (calculator.IsBusinessDay(today) ? 'is' : 'isn\'t') +' a business day');
+console.log('Today '+ (calculator.IsHoliday(today) ? 'is' : 'isn\'t') +' a holiday');
+console.log('The Carnival next year is due to '+ (calculator.NextHoliday(new Date("1 January "+today.getFullYear()))));
